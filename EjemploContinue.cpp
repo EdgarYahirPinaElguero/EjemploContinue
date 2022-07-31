e6507216aea32927116bd7211489626904722dc7
EdgarYahirPinaElguero <89890681+EdgarYahirPinaElguero@users.noreply.github.com>
2022-07-30 19:01:57 -0500

    Create EjemploContinue.cpp

---
+++ EjemploContinue.cpp
@@ -0,0 +1,13 @@
+package BreakContinue;
+
+public class Ejemplo1continue {
+	public static void main (String[] args) {
+		for(int i=1; i<=50; i++) {
+			if(i % 3 == 0 && i % 5==0) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		System.out.println("Hasta pronto");
+	}
+}
